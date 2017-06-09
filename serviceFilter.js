11fbba910f9145575ffc737f748dee7f2bf8b6f3
Clifford Pereira <cliffordpereira02@gmail.com>
2017-06-09 12:03:51 +0530

    msg

--- serviceFilter.js
+++ serviceFilter.js
@@ -79,7 +79,7 @@ function emailRead(model){
              if (body){
                      try{
 //                        model.info=JSON.parse(body)
-                        body=JSON.parse(body.data)
+                        body=JSON.parse(body)
                          console.log(body+"READ EMAIL BODY")
                          if(body.data.length==0&&body.data instanceof Array){
                              console.log("MAIL ID DOES NOT EXIST")
