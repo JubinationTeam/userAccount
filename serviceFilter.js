e92cc21a0e6f8ce8cdbee7ef93b61d64f251da80
Clifford Pereira <cliffordpereira02@gmail.com>
2017-06-09 13:55:35 +0530

    msg

--- serviceFilter.js
+++ serviceFilter.js
@@ -79,7 +79,7 @@ function emailRead(model){
                          body=JSON.parse(body)
                        
                          if(body.data.length>0&&(!!body.data) && (body.data.constructor === Array)){
-                             console.log("MAIL ID DOES EXIST")
+                             console.log("MAIL ID DOES EXIST"+body.data[0])
                              model.email=true
                          }
                          else{
@@ -140,7 +140,7 @@ function mobileRead(model){
                      try{ 
                         body=JSON.parse(body)
                          if(body.data.length>0&&(!!body.data) && (body.data.constructor === Array)){
-                             console.log("MOBILE DOES EXIST")
+                             console.log("MOBILE DOES EXIST"++body.data[0])
                              model.mobile=true
                              serviceCallDecision(model)
                          }
