244a93c93aa5a3b1a65e90543c0930724a1cc53b
Clifford Pereira <cliffordpereira02@gmail.com>
2017-06-09 12:56:56 +0530

    msg

--- serviceFilter.js
+++ serviceFilter.js
@@ -84,7 +84,7 @@ function emailRead(model){
 //                         console.log(body.data[0].email+" READ EMAIL BODY")
 //                         console.log(body.data.length+" LENGTH")
 //                         console.log(body.data instanceof Array+" BOOLEAN")
-                         console.log(body.data.isArray=== "[object Array]")
+                         console.log(body.data=== "[object Array]")
                          if(body.data.length==0&&body.data instanceof Array){
                              console.log("MAIL ID DOES NOT EXIST")
                              model.email=false
